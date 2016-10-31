4017ad577491783ab8b2b65e00bc13ca8f28e16f
yoshi <yoshi@DESKTOP-GV7AS6I>
2016-10-31 20:33:59 -0200

    Add project files.

---
+++ ML_Adgovados/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ML_Adgovados.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Teste()
+        {
+            return View();
+        }
+        public ActionResult Socios()
+        {
+            return View();
+        }
+        public ActionResult QuemSomos()
+        {
+            return View();
+        }
+        public ActionResult Areas()
+        {
+            return View();
+        }
+        public ActionResult AreaDetalhe()
+        {
+            return View();
+        }
+        public ActionResult SocioDetalhe()
+        {
+            return View();
+        }
+    }
+}
---
+++ ML_Adgovados/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ML_Adgovados.MvcApplication" Language="C#" %>
