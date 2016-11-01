1719bd97d9c54ca21e6455cf00ee48829dcf8f9c
yoshi <yoshi@DESKTOP-GV7AS6I>
2016-11-01 04:03:32 -0200

    95% Faltando apenas pente fino

--- ML_Adgovados/Controllers/HomeController.cs
+++ ML_Adgovados/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace ML_Adgovados.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Contato()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
@@ -43,11 +34,23 @@ namespace ML_Adgovados.Controllers
         {
             return View();
         }
-        public ActionResult AreaDetalhe()
+        public ActionResult Area()
         {
             return View();
         }
-        public ActionResult SocioDetalhe()
+        public ActionResult Socio()
+        {
+            return View();
+        }
+        public ActionResult Artigos()
+        {
+            return View();
+        }
+        public ActionResult Artigo()
+        {
+            return View();
+        }
+        public ActionResult Noticias()
         {
             return View();
         }
